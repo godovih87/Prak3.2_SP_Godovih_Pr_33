931f24a8e3357d3075d8a41c9add207891abbdd8
godovih87 <157564259+godovih87@users.noreply.github.com>
2024-10-05 13:03:16 +0500

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,60 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+struct Node {
+    int x, y;
+    Node* next;
+    Node(int _x, int _y) : x(_x), y(_y), next(nullptr) {}
+};
+
+class Snake {
+public:
+    Snake(int start_x, int start_y) {
+        Node* head = new Node(start_x, start_y);
+        Node* tail = head;
+        int length;
+        length = 1;
+    }
+    int foodx, foody;
+
+    void create_food(int max_x, int max_y){
+        /*foodx = rand() % 20
+        foody = rand() % 10*/
+    }
+
+    void draw_game(int max_x, int max_y) {
+        system("cls");
+
+        for (int x = 0; x < max_x + 2; x++) {
+            std::cout << "#";
+        }
+        std::cout << std::endl;
+
+        for (int y = 0; y < max_y; y++) {
+            std::cout << "#";
+            for (int x = 0; x < max_x; x++) {
+                std::cout << " ";
+            }
+            std::cout << "#" << std::endl;
+        }
+
+        for (int x = 0; x < max_x + 2; x++) {
+            std::cout << "#";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    int max_x = 20;
+    int max_y = 10;
+    Snake snake(max_x / 2, max_y / 2);
+    while (true) {
+        snake.draw_game(max_x, max_y);
+        Sleep(100);
+    }
+    return 0;
+}
